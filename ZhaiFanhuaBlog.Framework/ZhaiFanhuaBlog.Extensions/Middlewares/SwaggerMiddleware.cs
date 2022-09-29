d6ba0fef3bd7f3d028af78d881cc2bf0b61e68e1
ZhaiFanhua <zhaifanhua@foxmail.com>
2022-09-29 16:10:15 +0800

    优化：目录；

--- ZhaiFanhuaBlog.Framework/ZhaiFanhuaBlog.Extensions/Middlewares/SwaggerMiddleware.cs
+++ ZhaiFanhuaBlog.Framework/ZhaiFanhuaBlog.Extensions/Middlewares/SwaggerMiddleware.cs
@@ -57,7 +57,7 @@ public static class SwaggerMiddleware
             // API文档仅展开标记 List：列表式（展开子类），默认值;Full：完全展开;None：列表式（不展开子类）
             options.DocExpansion(DocExpansion.None);
             // 模型的默认扩展深度，设置为 -1 完全隐藏模型
-            options.DefaultModelsExpandDepth(4);
+            options.DefaultModelsExpandDepth(-1);
             // API前缀设置为空
             options.RoutePrefix = string.Empty;
         });
