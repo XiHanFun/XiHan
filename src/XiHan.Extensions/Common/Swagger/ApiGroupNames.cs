7a84357b5219ea1d76b2914802fb3ab176ef7905
ZhaiFanhua <zhaifanhua@foxmail.com>
2023-04-12 00:28:50 +0800

    优化：去除Swagger版本日期信息；

--- src/XiHan.Extensions/Common/Swagger/ApiGroupNames.cs
+++ src/XiHan.Extensions/Common/Swagger/ApiGroupNames.cs
@@ -21,36 +21,36 @@ public enum ApiGroupNames
     /// <summary>
     /// 所有接口
     /// </summary>
-    [GroupInfo(Title = "所有接口", Description = "这是用于生成调试接口的博客所有接口", Version = "v0.2.5-20221224")]
+    [GroupInfo(Title = "所有接口", Description = "这是用于生成调试接口的博客所有接口", Version = "v0.2.5")]
     All,
 
     /// <summary>
     /// 前台接口
     /// </summary>
-    [GroupInfo(Title = "前台接口", Description = "这是用于普通用户浏览的博客前台接口", Version = "v0.2.5-20221224")]
+    [GroupInfo(Title = "前台接口", Description = "这是用于普通用户浏览的博客前台接口", Version = "v0.2.5")]
     Reception,
 
     /// <summary>
     /// 后台接口
     /// </summary>
-    [GroupInfo(Title = "后台接口", Description = "这是用于管理的博客后台接口", Version = "v0.2.5-20221224")]
+    [GroupInfo(Title = "后台接口", Description = "这是用于管理的博客后台接口", Version = "v0.2.5")]
     Backstage,
 
     /// <summary>
     /// 授权接口
     /// </summary>
-    [GroupInfo(Title = "授权接口", Description = "这是用于登录的博客授权接口", Version = "v0.2.5-20221224")]
+    [GroupInfo(Title = "授权接口", Description = "这是用于登录的博客授权接口", Version = "v0.2.5")]
     Authorize,
 
     /// <summary>
     /// 公共接口
     /// </summary>
-    [GroupInfo(Title = "公共接口", Description = "这是用于常用功能的公共接口", Version = "v0.2.5-20221224")]
+    [GroupInfo(Title = "公共接口", Description = "这是用于常用功能的公共接口", Version = "v0.2.5")]
     Common,
 
     /// <summary>
     /// 测试接口
     /// </summary>
-    [GroupInfo(Title = "测试接口", Description = "这是用于测试的博客测试接口", Version = "v0.2.5-20221224")]
+    [GroupInfo(Title = "测试接口", Description = "这是用于测试的博客测试接口", Version = "v0.2.5")]
     Test,
 }
