0120d2f7c1e59895233d747b391855a67923931d
zhaifanhua <zhaifanhua@foxmail.com>
2023-05-31 17:52:48 +0800

    优化分页；

--- src/XiHan.Infrastructures/Responses/Pages/BasePageDto.cs
+++ src/XiHan.Infrastructures/Responses/Pages/BasePageDto.cs
@@ -24,9 +24,20 @@ public class BasePageDto
     public int CurrentIndex { get; set; } = 1;
 
     /// <summary>
+    /// 每页大小最大值
+    /// </summary>
+    public const int MaxPageSize = 100;
+
+    private int _pageSize = 10;
+
+    /// <summary>
     /// 每页大小
     /// </summary>
-    public int PageSize { set; get; } = 20;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
 }
 
 /// <summary>
