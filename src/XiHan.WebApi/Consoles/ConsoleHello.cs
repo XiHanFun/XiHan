a10a22a18c331e7e57e6a72bc48911638e0f9c80
zhaifanhua <zhaifanhua@foxmail.com>
2023-07-21 05:17:35 +0800

    🎨：优化欢迎语；

--- src/XiHan.WebApi/Consoles/ConsoleHello.cs
+++ src/XiHan.WebApi/Consoles/ConsoleHello.cs
@@ -12,6 +12,7 @@
 
 #endregion <<版权版本注释>>
 
+using XiHan.Infrastructures.Infos;
 using XiHan.Utils.Extensions;
 
 namespace XiHan.WebApi.Consoles;
@@ -26,47 +27,11 @@ public static class ConsoleHello
     /// </summary>
     public static void SayHello()
     {
-        Logo.WriteLineHandle();
-        SendWord.WriteLineHandle();
-        Copyright.WriteLineHandle();
-        OfficialDocuments.WriteLineHandle();
-        OfficialOrganization.WriteLineHandle();
-        SourceCodeRepository.WriteLineHandle();
+        HelloInfoHelper.Logo.WriteLineHandle();
+        HelloInfoHelper.SendWord.WriteLineHandle();
+        HelloInfoHelper.Copyright.WriteLineHandle();
+        $@"官方文档：{HelloInfoHelper.OfficialDocuments}".WriteLineHandle();
+        $@"官方组织：{HelloInfoHelper.OfficialOrganization}".WriteLineHandle();
+        $@"源码仓库：{HelloInfoHelper.SourceCodeRepository}".WriteLineHandle();
     }
-
-    /// <summary>
-    /// Logo
-    /// </summary>
-    private const string Logo = $@"
-██╗  ██╗██╗██╗  ██╗ █████╗ ███╗   ██╗
-╚██╗██╔╝██║██║  ██║██╔══██╗████╗  ██║
- ╚███╔╝ ██║███████║███████║██╔██╗ ██║
- ██╔██╗ ██║██╔══██║██╔══██║██║╚██╗██║
-██╔╝ ██╗██║██║  ██║██║  ██║██║ ╚████║
-╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝";
-
-    /// <summary>
-    /// 寄语
-    /// </summary>
-    private const string SendWord = $@"碧落降恩承淑颜，共挚崎缘挽曦寒。迁般故事终成忆，谨此葳蕤换思短。";
-
-    /// <summary>
-    /// Copyright
-    /// </summary>
-    private static readonly string Copyright = $@"Copyright (C){DateTime.Now.Year} ZhaiFanhua All Rights Reserved.";
-
-    /// <summary>
-    /// 官方文档
-    /// </summary>
-    private const string OfficialDocuments = $@"官方文档：https://docs.xihan.fun";
-
-    /// <summary>
-    /// 官方组织
-    /// </summary>
-    private const string OfficialOrganization = $@"官方组织：https://github.com/XiHanFun";
-
-    /// <summary>
-    /// 源码仓库
-    /// </summary>
-    private const string SourceCodeRepository = $@"源码仓库：https://github.com/XiHanFun/XiHan.Framework";
 }
---
+++ src/XiHan.WebApi/Controllers/Common/HelloController.cs
@@ -0,0 +1,48 @@
+﻿#region <<版权版本注释>>
+
+// ----------------------------------------------------------------
+// Copyright ©2023 ZhaiFanhua All Rights Reserved.
+// Licensed under the MulanPSL2 License. See LICENSE in the project root for license information.
+// FileName:HelloController
+// Guid:1ba84471-fadd-4d4c-94d6-04942cf3b4a1
+// Author:zhaifanhua
+// Email:me@zhaifanhua.com
+// CreateTime:2023/7/21 4:53:45
+// ----------------------------------------------------------------
+
+#endregion <<版权版本注释>>
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using XiHan.Application.Common.Swagger;
+using XiHan.Infrastructures.Infos;
+using XiHan.Infrastructures.Responses.Results;
+using XiHan.WebApi.Controllers.Bases;
+
+namespace XiHan.WebApi.Controllers.Common;
+
+/// <summary>
+/// 快速开始
+/// </summary>
+[AllowAnonymous]
+[ApiGroup(ApiGroupNames.Common)]
+public class HelloController : BaseApiController
+{
+    /// <summary>
+    /// 欢迎使用曦寒
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("SayHello")]
+    public CustomResult SayHello()
+    {
+        return CustomResult.Success(new
+        {
+            Hello = "欢迎使用曦寒，一款新型全场景应用软件，基于 DotNet 和 Vue 构建。",
+            HelloInfoHelper.SendWord,
+            HelloInfoHelper.Copyright,
+            HelloInfoHelper.OfficialDocuments,
+            HelloInfoHelper.OfficialOrganization,
+            HelloInfoHelper.SourceCodeRepository,
+        });
+    }
+}
