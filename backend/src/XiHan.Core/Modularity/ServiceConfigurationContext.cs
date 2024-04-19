231f79b624983e64391bfeeea0f939902846d7c3
zhaifanhua <zhaifanhua@foxmail.com>
2024-04-19 21:58:22 +0800

    优化程序包发布配置； 调整解决方案位置；

--- backend/src/XiHan.Core/Modularity/ServiceConfigurationContext.cs
+++ backend/src/XiHan.Core/Modularity/ServiceConfigurationContext.cs
@@ -12,6 +12,7 @@
 
 #endregion <<版权版本注释>>
 
+using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics.CodeAnalysis;
 
 namespace XiHan.Core.Modularity;
@@ -30,19 +31,20 @@ public class ServiceConfigurationContext
     /// the service registration phase and shared between modules.
     ///
     /// This is a shortcut usage of the <see cref="Items"/> dictionary.
+    ///
     /// Returns null if given key is not found in the <see cref="Items"/> dictionary.
     /// </summary>
     /// <param name="key"></param>
     /// <returns></returns>
-    public object? this[string key]
-    {
-        get => Items.GetOrDefault(key);
-        set => Items[key] = value;
-    }
+    //public object? this[string key]
+    //{
+    //    get => Items.GetOrDefault(key);
+    //    set => Items[key] = value;
+    //}
 
-    public ServiceConfigurationContext([NotNull] IServiceCollection services)
-    {
-        Services = Check.NotNull(services, nameof(services));
-        Items = new Dictionary<string, object?>();
-    }
+    //public ServiceConfigurationContext([NotNull] IServiceCollection services)
+    //{
+    //    Services = Check.NotNull(services, nameof(services));
+    //    Items = new Dictionary<string, object?>();
+    //}
 }
