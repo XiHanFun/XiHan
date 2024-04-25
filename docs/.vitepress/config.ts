0cb7e3606e96788a1f8abc8da8859aa788579926
zhaifanhua <zhaifanhua@foxmail.com>
2024-04-25 15:19:16 +0800

    网站同步仓库版权年份。

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -119,7 +119,7 @@ export default defineConfig({
       message:
         "Released under the <a href='http://license.coscl.org.cn/MulanPSL2' target='_blank'>MulanPSL2</a> License.",
       copyright:
-        "Copyright ©2023 <a href='https://www.zhaifanhua.com' target='_blank'>ZhaiFanhua</a> All Rights Reserved.",
+        "Copyright ©2021-2024 <a href='https://www.zhaifanhua.com' target='_blank'>ZhaiFanhua</a> All Rights Reserved.",
     },
   },
 });
